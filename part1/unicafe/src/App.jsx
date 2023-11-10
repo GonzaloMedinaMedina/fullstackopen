a15d1359f7c829bb5d4635ff6c16553f39824831
Gonzalo Medina Medina <gonzalomedinamedina@gmail.com>
2023-11-10 10:16:15 +0100

    Exercise 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -18,6 +18,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {(good - bad) / all}</p>
+      <p>positive {(good / all) * 100} %</p>
     </div>
   )
 }
