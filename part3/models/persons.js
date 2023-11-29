19bd8681ad8206d3c2067e9d5bf84856b347b7a7
Gonzalo Medina Medina <gonzalomedinamedina@gmail.com>
2023-11-29 10:05:14 +0100

    Exercise 2.20

--- part3/models/persons.js
+++ part3/models/persons.js
@@ -19,7 +19,14 @@ const personSchema = new mongoose.Schema({
       minLength: 3,
       required: true
     },
-    number: String,
+    number: {
+      type: String,
+      minLength: 8,
+      required: true,
+      validate: {
+        validator: (v) => { return /(\d{3}|\d{2})-\d*/.test(v); }
+      }
+    }
 })  
 
 personSchema.set('toJSON', {
