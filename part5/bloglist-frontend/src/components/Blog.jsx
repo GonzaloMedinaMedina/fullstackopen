6763cb1c0796532ad73aebf30863e7bf61a80ee7
Gonzalo Medina Medina <gonzalomedinamedina@gmail.com>
2023-12-13 13:17:14 +0900

    Exercise 5.7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,38 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => 
+{
+  const [visible, setVisible] = useState(false)
+
+  const hideWhenVisible = { display: visible ? 'none' : '' }
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const changeVisibilityButton = <button onClick={(e) => { toggleVisibility() }}>{visible ? 'hide' : 'view'}</button>
+
+  return <div style={blogStyle}>
+    <div style={hideWhenVisible}>
+      {blog.title} {blog.author}
+      {changeVisibilityButton}
+    </div>  
+    <div style={showWhenVisible}>
+      <div>{blog.title}{changeVisibilityButton}</div>
+      <div>{blog.url}</div>
+      <div>{blog.likes} <button>like</button></div>
+      <div>{blog.author}</div>
+    </div>
+  </div>
+}
 
 export default Blog
