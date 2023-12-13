831497f48f7461042f1a5fe5aa419d1f3b37a3f4
Gonzalo Medina Medina <gonzalomedinamedina@gmail.com>
2023-12-13 15:04:29 +0900

    Exercise 5.10

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -74,6 +74,12 @@ const App = () => {
     }
   }
 
+  const blogsCompoents = useMemo(
+    () => blogs
+      .sort((a, b) => b.likes - a.likes)
+      .map(blog =><Blog key={blog.id} blog={blog}/>)        
+    , [blogs])
+
   if (user !== '')
   {
     return (
@@ -86,9 +92,7 @@ const App = () => {
         <Togglable buttonLabel="new blog" ref={blogFormRef}>
           <CreateBlog createBlog={createBlog}/>
         </Togglable>
-        {blogs.map(blog =>
-          <Blog key={blog.id} blog={blog} />
-        )}
+        {blogsCompoents}
       </div>
     )
   }
