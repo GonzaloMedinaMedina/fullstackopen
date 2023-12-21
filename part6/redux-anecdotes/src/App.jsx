f80305704661c68572c8dd5346691708bfdc1e93
Gonzalo Medina Medina <gonzalomedinamedina@gmail.com>
2023-12-21 13:47:01 +0900

    Exercise 6.14

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -2,8 +2,22 @@ import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import Filter from './components/Filter'
 import Notification from './components/Notification'
+import { useEffect } from 'react'
+import anecdotesService from './services/anecdotesService'
+import { useDispatch } from 'react-redux'
 
 const App = () => {
+
+  const dispatch = useDispatch()
+
+  useEffect(() => 
+  {
+    anecdotesService.getAll().then(anecdotes => 
+    {
+      dispatch({ type: 'anecdotes/set', payload: anecdotes})
+    })
+  }, [])
+
   return (
     <div>
       <h2>Anecdotes</h2>
