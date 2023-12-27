ebf36c547f6b8b8c5aefa87aa3c57289ee357612
Gonzalo Medina Medina <gonzalomedinamedina@gmail.com>
2023-12-27 19:12:37 +0900

    Exercise 7.8

---
+++ part7/ultimate-hooks/src/hooks/index.js
@@ -0,0 +1,50 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+export const useField = (type) => 
+{
+    const [value, setValue] = useState('')
+  
+    const onChange = (event) => 
+    {
+        setValue(event.target.value)
+    }
+    
+    const reset = () => 
+    {
+        setValue('')
+    }
+    
+    return {
+      type,
+      value,
+      onChange,
+      reset
+    }
+}
+
+export const useResource = (url) => 
+{
+    const [value, setValue]= useState([])
+
+    useEffect(() => 
+    {
+        axios
+            .get(url)
+            .then(response => 
+            {
+                setValue(response.data)                
+            })
+            .catch(e => 
+            {
+            })
+    }, [])
+
+    const create = async (item) => 
+    {
+        const response = await axios.post(url, item)
+        return setValue(value.concat(response.data))
+    }
+
+    return [value, {create}]
+}
