4166861890a7fd92aa1bb77d7d3926122f8981b3
Gonzalo Medina Medina <gonzalomedinamedina@gmail.com>
2023-12-27 17:52:18 +0900

    Exercise 7.7

--- part7/routed-anecdotes/src/components/CreateAnecdote.jsx
+++ part7/routed-anecdotes/src/components/CreateAnecdote.jsx
@@ -33,15 +33,27 @@ const CreateAnecdote = (props) => {
         <form onSubmit={handleSubmit}>
           <div>
             content
-            <input {...content} />
+            <input 
+              value={content.value} 
+              type={content.type}
+              onChange={content.onChange}
+            />
           </div>
           <div>
             author
-            <input {...author} />
+            <input
+              value={author.value} 
+              type={author.type}
+              onChange={author.onChange}
+            />
           </div>
           <div>
             url for more info
-            <input {...info} />
+            <input
+              value={info.value} 
+              type={info.type}
+              onChange={info.onChange}
+            />
           </div>
           <button>create</button>
           <button type="button" onClick={handleReset}>reset</button>
