5246a86db5cdd7604dd0ff3593ca3789e0d2b8ae
Gonzalo Medina Medina <gonzalomedinamedina@gmail.com>
2024-01-04 17:08:26 +0900

    Exercise 7.9

--- part7/ultimate-hooks/src/hooks/index.js
+++ part7/ultimate-hooks/src/hooks/index.js
@@ -1,50 +1,41 @@
-import { useState, useEffect } from 'react'
-import axios from 'axios'
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-export const useField = (type) => 
-{
-    const [value, setValue] = useState('')
-  
-    const onChange = (event) => 
-    {
-        setValue(event.target.value)
-    }
-    
-    const reset = () => 
-    {
-        setValue('')
-    }
-    
-    return {
-      type,
-      value,
-      onChange,
-      reset
-    }
-}
+export const useField = (type) => {
+  const [value, setValue] = useState("");
 
-export const useResource = (url) => 
-{
-    const [value, setValue]= useState([])
+  const onChange = (event) => {
+    setValue(event.target.value);
+  };
 
-    useEffect(() => 
-    {
-        axios
-            .get(url)
-            .then(response => 
-            {
-                setValue(response.data)                
-            })
-            .catch(e => 
-            {
-            })
-    }, [])
+  const reset = () => {
+    setValue("");
+  };
 
-    const create = async (item) => 
-    {
-        const response = await axios.post(url, item)
-        return setValue(value.concat(response.data))
-    }
+  return {
+    type,
+    value,
+    onChange,
+    reset,
+  };
+};
 
-    return [value, {create}]
-}
+export const useResource = (url) => {
+  const [value, setValue] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(url)
+      .then((response) => {
+        setValue(response.data);
+      })
+      .catch((e) => {});
+  }, []);
+
+  const create = async (item) => {
+    const response = await axios.post(url, item);
+    return setValue(value.concat(response.data));
+  };
+
+  return [value, { create }];
+};
